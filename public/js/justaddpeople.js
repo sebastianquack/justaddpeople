63694c243cad5c5c7ae2f3ebc2efd6b617bc10fa
Sebastian Quack <sebastianquack@gmail.com>
2016-12-22 15:58:19 +0100

    diagonals css

--- public/js/justaddpeople.js
+++ public/js/justaddpeople.js
@@ -17,7 +17,8 @@ $(function() {
 
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
-    target: '.navbar-fixed-top'
+    target: '.navbar-fixed-top',
+    offset: 60
 })
 
 // Closes the Responsive Menu on Menu Item Click
