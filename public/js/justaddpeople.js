cbc970a5858373807f8e5c51769d11143ea55714
Sebastian Quack <sebastianquack@gmail.com>
2016-12-22 22:18:45 +0100

    add slick project caroussel

--- public/js/justaddpeople.js
+++ public/js/justaddpeople.js
@@ -13,6 +13,41 @@ $(function() {
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
+    
+    
+    $('.slick').slick({
+      arrows: true,
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
 });
 
 // Highlight the top nav as scrolling occurs
