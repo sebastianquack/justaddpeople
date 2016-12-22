c1066c17cab8af8762464beba5983faf2380128e
Sebastian Quack <sebastianquack@gmail.com>
2016-12-22 12:18:26 +0100

    asset config in production

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,16 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
+Rails.application.config.assets.paths << "#{Rails.root}/app/assets/fonts"
+Rails.application.config.assets.paths << "#{Rails.root}/app/assets/images"
+
+Dir.glob("#{Rails.root}/app/assets/images/**/").each do |path|
+  Rails.application.config.assets.paths << path
+end
+
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+#
+Rails.application.config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif *.otf *.ttf] 
